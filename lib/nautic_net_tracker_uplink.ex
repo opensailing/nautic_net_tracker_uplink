c4bcfb1d8d0dc42ab19d92e3b9d45027fbbdcd76
Rockwell Schrock <rockwell@schrock.me>
2023-04-06 15:31:08 -0400

    `mix nerves.new`

---
+++ lib/nautic_net_tracker_uplink.ex
@@ -0,0 +1,18 @@
+defmodule NauticNetTrackerUplink do
+  @moduledoc """
+  Documentation for NauticNetTrackerUplink.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NauticNetTrackerUplink.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nautic_net_tracker_uplink_test.exs
@@ -0,0 +1,8 @@
+defmodule NauticNetTrackerUplinkTest do
+  use ExUnit.Case
+  doctest NauticNetTrackerUplink
+
+  test "greets the world" do
+    assert NauticNetTrackerUplink.hello() == :world
+  end
+end
